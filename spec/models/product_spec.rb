595cd637aaf7aa5cb127a1ce6d4ba9032b4648ab
alisa-c <alisachuvilskaya@gmail.com>
2018-12-10 21:06:38 +0000

    product testing edited

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,31 +4,31 @@ RSpec.describe Product, type: :model do
 
   describe 'Validations' do
     before(:each) do
-      @category = Category.new(:name => "something")
+      @category = Category.create(:name => "something")
     end
 
       it "is valid with valid attributes" do
-        @product = Product.new(name: "some_name", price: 5, quantity: 1, category: @category)
+        @product = Product.create(name: "some_name", price: 248, quantity: 49, category: @category)
         expect(@product).to be_valid
       end
 
       it "is not valid without a name" do
-        @product = Product.new(name: nil, price: 5, quantity: 1, category: @category)
+        @product = Product.create(name: nil, price: 394, quantity: 290, category: @category)
         expect(@product).to_not be_valid
       end
 
       it "is not valid without a price" do
-        @product = Product.new(name: "some_name", price: nil, quantity: 1, category: @category)
+        @product = Product.create(name: "name", price: nil, quantity: 943, category: @category)
         expect(@product).to_not be_valid
       end
 
       it "is not valid without a quantity" do
-        @product = Product.new(name: "some_name", price_cents: 5, quantity: nil, category: @category)
+        @product = Product.create(name: "what_name", price: 3493, quantity: nil, category: @category)
         expect(@product).to_not be_valid
       end
 
       it "is not valid without a category" do
-        @product = Product.new(name: "some_name", price_cents: 5, quantity: 1, category: nil)
+        @product = Product.create(name: "some", price: 100, quantity: 2, category: nil)
         expect(@product).to_not be_valid
       end
   end
